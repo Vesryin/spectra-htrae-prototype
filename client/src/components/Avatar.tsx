1fc9ed39300229ff2e73a1f1672ee150520d2bef
Richard Jacob Olejniczak <vesryin@gmail.com>
2025-08-13 22:19:54 -0600

    Create Avatar.tsx

---
+++ client/src/components/Avatar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Mood, getMoodColor, getMoodEmoji } from "../utils/moods";
+
+interface AvatarProps {
+  mood: Mood;
+  position: { x: number; y: number };
+}
+
+export default function Avatar({ mood, position }: AvatarProps) {
+  return (
+    <div
+      className={`absolute w-12 h-12 rounded-full border-2 border-indigo-400 flex items-center justify-center text-white shadow-lg`}
+      style={{
+        top: `${position.y}%`,
+        left: `${position.x}%`,
+        backgroundColor: getMoodColor(mood),
+        transition: "all 1s ease"
+      }}
+    >
+      {getMoodEmoji(mood)}
+    </div>
+  );
+}
